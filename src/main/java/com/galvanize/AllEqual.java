2f5c885ec129371ae25b6c2d40074aff15dc0dcd
Thacher Hussain <thachert1d@gmail.com>
2018-04-04 14:13:05 -0700

    AllEqual.java added

---
+++ src/main/java/com/galvanize/AllEqual.java
@@ -0,0 +1,14 @@
+package com.galvanize;
+
+public class AllEqual {
+
+    boolean allEqual(String input) {
+        // real stuff
+        return false;
+    }
+
+    public static void main(String[] args) {
+        AllEqual object = new AllEqual();
+        System.out.println(object.allEqual("hello"));
+    }
+}
